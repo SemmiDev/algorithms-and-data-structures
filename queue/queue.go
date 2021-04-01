e1d4184140702c0f84ca870e16edf34df8fc6671
SemmiDev <sammidev4@gmail.com>
2021-04-01 20:37:33 +0700

    queue

---
+++ queue/queue.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type Queue []interface{}
+
+type QueueOperations interface {
+	IsEmpty() bool
+	size() int
+	enqueue(value interface{})
+	dequeue()
+}
+
+func NewQueue() QueueOperations {
+	return &Queue{}
+}
+
+func (q Queue) IsEmpty() bool {
+	panic("implement me")
+}
+
+func (q *Queue) size() int {
+	return len(*q)
+}
+
+func (q *Queue) enqueue(value interface{}) {
+	*q = append(*q, value)
+}
+
+func (q *Queue) dequeue() {
+	*q = (*q)[1:]
+}
+
+func main() {
+	queue := NewQueue()
+
+	queue.enqueue(1)
+	queue.enqueue(2)
+
+	fmt.Print(queue)
+	queue.dequeue()
+	fmt.Print(queue)
+
+	queue.enqueue(3)
+	queue.enqueue(4)
+	queue.enqueue(5)
+
+	fmt.Print(queue)
+	queue.dequeue()
+	fmt.Print(queue)
+}
